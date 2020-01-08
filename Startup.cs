c804e7f9148da21e19aea9d6884b6b386ed594d4
Esteban Varela Cartín <estcartin@gmail.com>
2020-01-08 08:29:35 -0600

    Added code first migrations and added the first migration.

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vega.Persistence;
 
 namespace Vega
 {
@@ -21,6 +23,9 @@ namespace Vega
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
